1b5c668b9b040993a6d78f7f616fa30b752f1344
Yzech <96177428+Yzech@users.noreply.github.com>
2021-12-16 23:12:18 +0800

    Update pickanddrop.py

--- pickanddrop.py
+++ pickanddrop.py
@@ -18,7 +18,10 @@ def pickup():
     # approach humanoid until it is minimum distance away
     print("approaching humanoid")
     minDist = 12 # todo: replace with math figure later
-    dist = ir_distance_sensor_ctrl.get_distance_info(1)
+    dist = ir_distance_sensor_ctrl.get_distance_info(1) 
+    calcDist = predict_dist()
+    if abs(dist-calcDist) >= 10: 
+        dist = calcDist
     while dist > minDist:
         chassis_ctrl.move(0)
         dist = ir_distance_sensor_ctrl.get_distance_info(1)
@@ -63,3 +66,16 @@ def dropoff():
     chassis_ctrl.rotate_with_time(rm_define.anticlockwise, 0.3)  # away from marker
 
     print("dropoff completed")
+    
+    
+    # following code assumes that u have already detected the vision marker and that the robot is stationary
+    
+def predict_dist():
+    s1 = vision_ctrl.get_marker_detection_info() #ir info in list s1
+    w1, h1 = s1[3], s1[4] #y coords, width
+    m = 10
+    chassis_ctrl.move_with_distance(0, m/100) #moves 10cm
+    s2 = vision_ctrl.get_marker_detection_info()
+    w2, h2 = s2[3], s2[4]
+    new_dist_from_obj = (((10*w2 / (w2-w1))-10)+((10*h2 / (h2-h1))-10))/2
+    return new_dist_from_obj
